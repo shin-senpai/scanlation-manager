0049b67da684482bdb7d412eb196daaaca429671
shin-senpai <shin_secure@proton.me>
2026-02-12 17:00:33 +0600

    Initialize Repository based on DPP provided templatebot

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <templatebot/templatebot.h>
+
+/* When you invite the bot, be sure to invite it with the
+ * scopes 'bot' and 'applications.commands', e.g.
+ * https://discord.com/oauth2/authorize?client_id=940762342495518720&scope=bot+applications.commands&permissions=139586816064
+ */
+
+using json = nlohmann::json;
+
+int main(int argc, char const *argv[]) {
+    json configdocument;
+    std::ifstream configfile("../config.json");
+    configfile >> configdocument;
+
+    const std::string BOT_TOKEN = configdocument["token"];
+
+    /* Set up the bot */
+    dpp::cluster bot(BOT_TOKEN);
+
+    /* Output simple log messages to stdout */
+    bot.on_log(dpp::utility::cout_logger());
+
+    /* Handle slash command */
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+         if (event.command.get_command_name() == "ping") {
+            event.reply("Pong!");
+        }
+    });
+
+    /* Register slash command here in on_ready */
+    bot.on_ready([&bot](const dpp::ready_t& event) {
+        /* Wrap command registration in run_once to make sure it doesn't run on every full reconnection */
+        if (dpp::run_once<struct register_bot_commands>()) {
+            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+        }
+    });
+
+    /* Start the bot */
+    bot.start(dpp::st_wait);
+
+    return 0;
+}

af9cc297985d1c9e27f3e550723d0f5aed77fc52
shin-senpai <shin_secure@proton.me>
2026-05-10 12:29:24 +0600

    fix: update Discord timestamp format to date-only

--- discord/src/bot/utils/DateUtils.cpp
+++ discord/src/bot/utils/DateUtils.cpp
@@ -9,5 +9,5 @@ std::string BotUtils::toDiscordTimestamp(const std::string &date) {
   std::tm tm{};
   strptime(date.c_str(), "%Y-%m-%d", &tm);
   const time_t t = timegm(&tm);
-  return "<t:" + std::to_string(static_cast<long long>(t)) + ">";
+  return "<t:" + std::to_string(static_cast<long long>(t)) + ":D>";
 }
